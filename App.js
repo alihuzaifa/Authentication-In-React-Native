655f003c30031975693ac178c481bfaeaa152fd3
alihuzaifa <ha4587323@gmail.com>
2022-12-24 11:16:55 +0500

    making front page ui design

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, Button } from 'react-native';
 
 const styles = StyleSheet.create({
   mainContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white"
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white'
   },
   logo: {
-    width: 200,
-    height: 200,
+    width: 300,
+    height: 300,
+    marginTop: 50,
+  },
+  head: {
+    color: 'orange',
+    fontSize: 28,
+    marginTop: -60,
+    fontWeight: 'bold',
+  },
+  title: {
+    fontSize: 17,
+    marginHorizontal: 20,
+    textAlign: 'center',
+  },
+  btnContainer: {
+    width: 270,
+    borderRadius: '100px',
+    marginVertical: 10
+  },
+  button: {
+    padding: 50,
   }
 });
 function App() {
@@ -18,6 +37,30 @@ function App() {
     <View style={styles.mainContainer}>
       <View>
         <Image source={require('./assets/image/lock.gif')} style={styles.logo} />
+      </View>
+      <View>
+        <Text style={styles.head}>
+          Login Template
+        </Text>
+      </View>
+      <View>
+        <Text style={styles.title}>
+          The easiest way to start with your amazing application.
+        </Text>
+      </View>
+      <View style={styles.btnContainer}>
+        <Button
+          style={styles.button, { marginTop: 20 }}
+          title='Login'
+          color='orange'
+        />
+      </View>
+      <View style={styles.btnContainer}>
+        <Button
+          style={styles.button}
+          title='Sign up'
+          color='orange'
+        />
       </View>
     </View>
   );
